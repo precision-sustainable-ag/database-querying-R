42701a958508762a57c29cd0d6773baafba7e564
brianwdavis <brianwdavis@gmail.com>
2019-12-28 17:49:12 -0500

    added library info to cleanup

--- 999_Cleanup.R
+++ 999_Cleanup.R
@@ -1,3 +1,5 @@
+# Anonymize HTML output
+
 fs::dir_ls(glob = "*.html") %>%
   purrr::walk(~{
     html_text <- readr::read_lines(.x) 
@@ -11,4 +13,22 @@ fs::dir_ls(glob = "*.html") %>%
     
     html_text[!flags] %>% 
       readr::write_lines(.x)
+  })
+
+# Find `library` calls
+fs::dir_ls(glob = "*.Rmd") %>% 
+  purrr::map(~{
+    rtext <- readr::read_lines(.x)
+    
+    libs <- stringr::str_extract_all(rtext, "library\\(.+\\)")
+    purrr::compact(libs) %>% unlist()
+  })
+
+# Find namespace calls
+fs::dir_ls(glob = "*.Rmd") %>% 
+  purrr::map(~{
+    rtext <- readr::read_lines(.x)
+    
+    colons <- stringr::str_extract_all(rtext, "\\b[A-z0-9]+::[.A-z0-9_]+")
+    purrr::compact(colons) %>% unlist() %>% sort()
   })
